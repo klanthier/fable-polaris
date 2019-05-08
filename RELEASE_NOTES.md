7367e50c732c46626cb536c7bbec1c3dd18b004a
Thiago Temple <thiagot@surveymonkey.com>
2019-05-08 13:53:10 -0400

    Removes dependency on paket and add a couple of components

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+### 0.1.0
+
+* AppProvider component bindings
+* Avatar component bindings
+* Page (basic) component bindings
+* Spinner component bindings
--- build.fsx
+++ build.fsx
@@ -1,55 +1,9 @@
-#r "paket: groupref netcorebuild //"
-#load ".fake/build.fsx/intellisense.fsx"
+#r "node_modules/fable-metadata/lib/Fable.Core.dll"
+#load "node_modules/fable-publish-utils/PublishUtils.fs"
+open PublishUtils
 
-#if !FAKE
-#r "Facades/netstandard"
-#r "netstandard"
-#endif
-
-open System
-open Fake.Core
-open Fake.Core.TargetOperators
-open Fake.DotNet
-open Fake.IO
-open Fake.IO.Globbing.Operators
-open Fake.JavaScript
-
-Target.create "Clean" (fun _ ->
-    !! "sample/src/**/bin"
-    ++ "sample/src/**/obj"
-    ++ "src/**/bin"
-    ++ "src/**/obj"
-    |> Seq.iter Shell.cleanDir
-)
-
-Target.create "Install" (fun _ ->
-    !! "src/**/*.fsproj"
-    ++ "sample/src/**/*.fsproj"
-    |> Seq.iter (fun s ->
-        let dir = IO.Path.GetDirectoryName s
-        DotNet.restore id dir)
-)
-
-Target.create "Build" (fun _ ->
-    !! "src/**/*.fsproj"
-    ++ "sample/src/**/*.fsproj"
-    |> Seq.iter (fun s ->
-        let dir = IO.Path.GetDirectoryName s
-        DotNet.build id dir)
-)
-
-Target.create "YarnInstall" (fun _ ->
-    Yarn.install id
-)
-
-
-// Build order
-"Clean"
-    ==> "Install"
-    ==> "Build"
-
-"Build"
-    ==> "YarnInstall"
-
-// start build
-Target.runOrDefault "Build"
+// run "npm test"
+match args with
+| IgnoreCase "publish"::_ ->
+    pushNuget "src/fable-polaris.fsproj"
+| _ -> ()
