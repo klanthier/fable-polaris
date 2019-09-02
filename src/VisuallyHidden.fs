73533ad696373d732d0805acc445bdd9b2418eab
Kevin Lanthier <5543043+klanthier@users.noreply.github.com>
2019-09-02 00:30:42 -0400

    Added VisuallyHidden & Topbar

---
+++ src/VisuallyHidden.fs
@@ -0,0 +1,5 @@
+module Polaris.VisuallyHidden
+open Fable.React
+
+let inline polarisVisuallyHidden (elems: ReactElement list): ReactElement =
+    ofImport "VisuallyHidden" "@shopify/polaris" [] elems
